ecffab1fbd5b189a80ac0b5d362c6c78614aa08d
龙骑士尹道长 <523580265@qq.com>
2023-06-30 10:27:14 +0800

    #pipeLine block   u

--- src/pages/Editor/components/FollowMenu/index.tsx
+++ src/pages/Editor/components/FollowMenu/index.tsx
@@ -1,25 +1,55 @@
-import React, {useContext} from "react";
-import './style.less'
-import {StoreContext} from "@/store";
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import './style.less';
+import { StoreContext } from '@/store';
 
-const FollowMenu:React.FC =()=>{
-  const {state} = useContext(StoreContext)
-  const {visible,x,y} = state.followMenuConfig
-  return visible ?(<div className="follow-menu" style={{left:x,top:y,}}>
-    <div className="group">
-      <div className="menu-group-item">置顶</div>
-      <div className="menu-group-item">置底</div>
-      <div className="menu-group-item">上移一层</div>
-      <div className="menu-group-item">下移一层</div>
+const FollowMenu: React.FC = () => {
+  const { state } = useContext(StoreContext);
+  const { visible, x, y } = state.followMenuConfig;
+  const menuRef = useRef(null);
+  const [pointX, setPointX] = useState(0);
+  const [pointY, setPointY] = useState(0);
+  useEffect(() => {
+    if (visible) {
+      if (menuRef.current) {
+        const { clientHeight, clientWidth } = menuRef.current;
+        let screenWidth = window.innerWidth;
+        let screenHeight = window.innerHeight;
+        setPointX(
+          clientWidth + parseInt(x) < screenWidth
+            ? x
+            : screenWidth - clientWidth,
+        );
+        setPointY(
+          clientHeight + parseInt(y) < screenHeight
+            ? y
+            : screenHeight - clientHeight,
+        );
+      }
+    }
+  }, [visible]);
+  return visible ? (
+    <div
+      className="follow-menu"
+      style={{ left: pointX, top: pointY }}
+      ref={menuRef}
+    >
+      <div className="group">
+        <div className="menu-group-item">置顶</div>
+        <div className="menu-group-item">置底</div>
+        <div className="menu-group-item">上移一层</div>
+        <div className="menu-group-item">下移一层</div>
+      </div>
+      <div className="group">
+        <div className="menu-group-item">锁定</div>
+        <div className="menu-group-item">拷贝</div>
+        <div className="menu-group-item">删除</div>
+        <div className="menu-group-item">成组</div>
+        <div className="menu-group-item">取消成组</div>
+      </div>
     </div>
-    <div className="group">
-      <div className="menu-group-item">锁定</div>
-      <div className="menu-group-item">拷贝</div>
-      <div className="menu-group-item">删除</div>
-      <div className="menu-group-item">成组</div>
-      <div className="menu-group-item">取消成组</div>
-    </div>
-  </div>):<></>
-}
+  ) : (
+    <></>
+  );
+};
 
-export default FollowMenu
+export default FollowMenu;
--- src/pages/Editor/index.tsx
+++ src/pages/Editor/index.tsx
@@ -8,8 +8,19 @@ import RightDrawer from '@/pages/Editor/plugin/RightDrawer';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { Store, StoreContext, TYPES } from '@/store';
+import { ConfigProvider, Modal, theme } from 'antd';
+
 const Editor: React.FC = () => {
   const { state, dispatch } = useContext(StoreContext);
+  const [modal, contextHolder] = Modal.useModal();
+  const confirm = () => {
+    modal.confirm({
+      title: '提示',
+      content: '是否删除',
+      okText: '确认',
+      cancelText: '取消',
+    });
+  };
   const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.ctrlKey && !state.isKeydownCtrlKey) {
       dispatch({
@@ -26,12 +37,15 @@ const Editor: React.FC = () => {
       });
     }
   };
+
+  // console.log(theme)
   useEffect(() => {
     //批量添加键盘输入事件，随组件销毁
     // @ts-ignore
     window.addEventListener('keydown', handleKeyDown);
     // @ts-ignore
     window.addEventListener('keyup', handleKeyUp);
+    confirm();
     return () => {
       // @ts-ignore
       window.removeEventListener('keydown', handleKeyDown);
@@ -40,21 +54,41 @@ const Editor: React.FC = () => {
     };
   }, []);
   return (
-    <DndProvider backend={HTML5Backend}>
-      <div className="editor-container page-root">
-        <HeaderBar />
-        <div className="content-view">
-          <LeftDrawer />
-          <div className="editor-view-container">
-            <div className="editor-view-box">
-              <EditorView />
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimaryBg: 'rgba(18,28,36,.45)',
+          colorBgContainer: 'rgb(18,28,36,.45)',
+          colorText: 'rgba(255, 255, 255, 0.88)',
+          colorTextSecondary: 'rgba(255, 255, 255, 0.65)',
+          colorTextTertiary: 'rgba(255,255,255,0.45)',
+          colorTextQuaternary: 'rgba(255, 255, 255, 0.25)',
+          colorFill: 'rgba(255,255,255,0.15)',
+          colorFillSecondary: 'rgba(255,255,255,0.06)',
+          colorFillTertiary: 'rgba(255,255,255,0.04)',
+          colorFillQuaternary: 'rgba(255,255,255,0.02)',
+          colorBgElevated: 'rgba(18,28,36,1)',
+          colorBgMask: 'rgba(0,0,0,0)',
+        },
+      }}
+    >
+      <DndProvider backend={HTML5Backend}>
+        <div className="editor-container page-root">
+          {contextHolder}
+          <HeaderBar />
+          <div className="content-view">
+            <LeftDrawer />
+            <div className="editor-view-container">
+              <div className="editor-view-box">
+                <EditorView />
+              </div>
+              <BottomBar />
             </div>
-            <BottomBar />
+            <RightDrawer />
           </div>
-          <RightDrawer />
         </div>
-      </div>
-    </DndProvider>
+      </DndProvider>
+    </ConfigProvider>
   );
 };
 export default () => {
